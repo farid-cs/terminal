82fa61305658d8c8fc5da925de1d4cf4167c2033
Farid <faridfaracli9@gmail.com>
2025-01-19 12:02:56 +0400

    store window name in a variable

--- terminal.c
+++ terminal.c
@@ -1,13 +1,14 @@
 #include <raylib.h>
 #include <stdlib.h>
 
+static const char WINDOW_NAME[] = "terminal"; 
 static const int WINDOW_WIDTH = 1600;
 static const int WINDOW_HEIGHT = 1200;
 
 int
 main(void)
 {
-	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "terminal");
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
