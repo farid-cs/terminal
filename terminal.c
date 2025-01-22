e51e7b333975ad64b0a9499cf7066964f44f8610
Farid <faridfaracli9@gmail.com>
2025-01-22 20:56:34 +0400

    rename a variable

--- terminal.c
+++ terminal.c
@@ -1,14 +1,14 @@
 #include <raylib.h>
 #include <stdlib.h>
 
-static const char WINDOW_NAME[] = "terminal";
+static const char WINDOW_TITLE[] = "terminal";
 static const int WINDOW_WIDTH = 1600;
 static const int WINDOW_HEIGHT = 1200;
 
 int
 main(void)
 {
-	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
