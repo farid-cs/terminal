e85c9f204675ae4cad689f846d94444ed8984e90
Farid <faridfaracli9@gmail.com>
2025-01-29 14:16:04 +0400

    spawn a child process and print message received from it

--- terminal.py
+++ terminal.py
@@ -1,19 +1,30 @@
 #!/bin/env python3
 
 import pyray as rl
+import subprocess
+import pty
+import os
 
 WINDOW_TITLE = 'terminal'
 WINDOW_WIDTH = 1600
 WINDOW_HEIGHT = 1200
 
 def main():
+    pid, fd = pty.fork()
+
+    if pid == 0:
+        subprocess.run(['printf', 'Hello from child process!'])
+        return
+
+    message = os.read(fd, 1024)
+
     rl.init_window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
     while not rl.window_should_close():
         rl.begin_drawing()
 
         rl.clear_background(rl.RAYWHITE)
-        rl.draw_text('Hello World!', 0, 0, 30, rl.BLACK)
+        rl.draw_text(message.decode(), 0, 0, 30, rl.BLACK)
 
         rl.end_drawing()
 
