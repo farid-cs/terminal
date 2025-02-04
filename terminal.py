a1113dd3743f6d4c172d363d7c5f27bd77bcffea
Farid <faridfaracli9@gmail.com>
2025-02-04 22:30:59 +0400

    on master side recieve all data till the EOF sent by the client side

--- terminal.py
+++ terminal.py
@@ -2,9 +2,13 @@
 
 import pyray as rl
 
+import errno
 import os
+import platform
 import pty
 import subprocess
+import sys
+import time
 
 WINDOW_TITLE = 'terminal'
 WINDOW_WIDTH = 1600
@@ -14,10 +18,21 @@ def start_terminal():
     pid, fd = pty.fork()
 
     if pid == 0:
+        # simulate work
+        time.sleep(2)
         subprocess.run(['printf', 'Hello from child process!'])
+        time.sleep(2)
+        subprocess.run(['printf', ' How are you?'])
+        time.sleep(2)
+
+        # cleanup
+        os.close(sys.stdin.fileno())
+        os.close(sys.stdout.fileno())
+        os.close(sys.stderr.fileno())
+
         exit()
 
-    message = os.read(fd, 1024)
+    message = b''
 
     rl.init_window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
@@ -28,6 +43,13 @@ def start_terminal():
         rl.draw_text(message.decode(), 0, 0, 30, rl.BLACK)
 
         rl.end_drawing()
+
+        try:
+            message += os.read(fd, 1024)
+        except OSError as err:
+            if err.errno == errno.EIO and platform.system() == 'Linux':
+                break
+            raise err
 
     rl.close_window()
 
